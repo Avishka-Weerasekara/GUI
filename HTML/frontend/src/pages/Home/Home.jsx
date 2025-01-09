04758fdb43e56260d65b60e53af316bf7eb0af29
Avishka Weerasekara <Buddhi00weerasekara@gmail.com>
2025-01-10 01:14:33 +0530

    edit home

--- HTML/frontend/src/pages/Home/Home.jsx
+++ HTML/frontend/src/pages/Home/Home.jsx
@@ -8,32 +8,31 @@ const Home = () => {
   return (
     <div>
         <div class="main-content">
-        <section class="home-section">
-            <div class="home-content">
-                <h1>Welcome to Bite & Bliss</h1>
-                <p>Experience the perfect blend of taste and luxury</p>
-            </div>
-        </section>
+            <section class="home-section">
+                <div class="home-content">
+                    <h1>Welcome to Bite & Bliss</h1>
+                    <p>Experience the perfect blend of taste and luxury</p>
+                </div>
+            </section>
         
-        <div class="container">
-            <div class="card">
-                <img src='./home_1.jpg' class="home1"/>
+            <div class="container">
+                <div class="card">
+                    <img src='./home_1.jpg' class="home1"/>
+                </div>
+                <div class="card">
+                    <img src='./home_2.jpg' class="home1"/>
+                </div>
+                <div class="card">
+                    <img src='./home_3.jpg' class="home1"/>
+                </div>
+                <div class="card">
+                    <img src='' class="home1"/>
+                </div>
+                <div class="card">
+                    <img src='' class="home1"/>
+                </div>
             </div>
-            <div class="card">
-                <img src='./home_2.jpg' class="home1"/>
-            </div>
-            <div class="card">
-                <img src='./home_3.jpg' class="home1"/>
-            </div>
-            <div class="card">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Al_acecho_%289272124788%29.jpg/1280px-Al_acecho_%289272124788%29.jpg"/>
-            </div>
-            <div class="card">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Mimi%26Tigsi.jpg/1280px-Mimi%26Tigsi.jpg"/>
-  </div>
-</div>
-    </div>
-      
+        </div>  
     </div>
   )
 }
