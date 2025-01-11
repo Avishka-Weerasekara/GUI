e0cf4596672c94138348182b89027706cbd81c01
Avishka Weerasekara <Buddhi00weerasekara@gmail.com>
2025-01-11 18:33:14 +0530

    edit menu page

--- HTML/frontend/src/pages/Menu/Menu.jsx
+++ HTML/frontend/src/pages/Menu/Menu.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Menu.css'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 const Menu = () => {
+
+        const [category,setCategory] = useState("All");
+
+
     return (
         <div>
             <div class="main-content-menu">
                 <section class="menu-section">
                     <div class="menu-content">
-                        <h1>Welcome to Bite & Bliss</h1>
+                        <h1>Menu</h1>
                         <p>Experience the perfect blend of taste and luxury</p>
                     </div>
+                    <ExploreMenu category={category} setCategory={setCategory}/>
                 </section>
     
                 
