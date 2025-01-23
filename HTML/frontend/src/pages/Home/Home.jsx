8f5d816d43bcbb7e25182adf59d992eab25d5f0d
Avishka Weerasekara <Buddhi00weerasekara@gmail.com>
2025-01-23 11:09:35 +0530

    edit

--- HTML/frontend/src/pages/Home/Home.jsx
+++ HTML/frontend/src/pages/Home/Home.jsx
@@ -10,6 +10,11 @@ function Home() {
       <div className="fn"><FN/></div>
       <div className="home-content">
         <h1><u>Welcome to Bliss & Bite</u></h1>
+        <p> 
+        Discover a world of flavors with our diverse menu, featuring Indian, Italian, Mexican,
+         Chinese cuisines, indulgent desserts, and refreshing beverages. Enjoy dishes crafted 
+         by our world-renowned chef and served by our friendly staff for an unforgettable dining 
+         experience!</p>
       </div>
       
       <HH/>
