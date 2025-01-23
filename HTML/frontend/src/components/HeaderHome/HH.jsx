26052e34d3cefa3b3ade8529c15a6bac7ce81444
Avishka Weerasekara <Buddhi00weerasekara@gmail.com>
2025-01-23 10:58:32 +0530

    edit home page

---
+++ HTML/frontend/src/components/HeaderHome/HH.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './HH.css'
+
+const HH = () => {
+  return (
+    <div>
+       <div class="container-1">
+                <div class="card">
+                    <img src='./indian-home.jpg' class="home1"/>
+                    <div class="card__head">Indian</div>
+                </div>
+                <div class="card">
+                    <img src='./italian-home.jpg' class="home1"/>
+                    <div class="card__head">Italian</div>
+                </div>
+                <div class="card">
+                    <img src='./chinese-home.jpg' class="home1"/>
+                    <div class="card__head">Chinese</div>
+                </div>
+                <div class="card">
+                    <img src='./mexican-home.jpg' class="home1"/>
+                    <div class="card__head">Mexican</div>
+                </div>
+                <div class="card">
+                    <img src='./dessert-home.jpg' class="home1"/>
+                    <div class="card__head">Desserts</div>
+                </div>
+                <div class="card">
+                    <img src='./beverages-home.jpg' class="home1"/>
+                    <div class="card__head">Beverages</div>
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default HH
--- HTML/frontend/src/pages/Home/Home.jsx
+++ HTML/frontend/src/pages/Home/Home.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import './Home.css'
 import FN from '../../components/FrontAName/FN'
 import Footer from '../../components/Footer/Footer'
+import HH from '../../components/HeaderHome/HH'
 
 function Home() {
   return (
-    <div>
-      <FN/>
+    <div class="home">
+      <div className="fn"><FN/></div>
+      <div className="home-content">
+        <h1><u>Welcome to Bliss & Bite</u></h1>
+      </div>
+      
+      <HH/>
       
     </div>
   )
